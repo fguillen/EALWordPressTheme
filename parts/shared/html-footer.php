82919b7c07614fd81c81190246970a13883841cf
Fernando Guillen <fguillen.mail@gmail.com>
2012-09-27 18:33:09 +0200

    analytics

--- parts/shared/html-footer.php
+++ parts/shared/html-footer.php
@@ -1,4 +1,16 @@
+    <?php wp_footer(); ?>
 
-	<?php wp_footer(); ?>
+    <!-- analytics -->
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-4035677-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
 	</body>
 </html>
