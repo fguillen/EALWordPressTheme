223e1ad4cc8936247807a4ba6af651f2110ddd76
Fernando Guillen <fguillen.mail@gmail.com>
2012-12-14 22:33:19 +0100

    favicon

--- parts/shared/html-header.php
+++ parts/shared/html-header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>escrito a lápiz | la web</title>
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
+		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.ico"/>
 		<link rel='stylesheet' href='<?php bloginfo('template_directory'); ?>/css/reset.css' type='text/css' />
 
     <?php wp_head(); ?>
